5af0a2ac4cd2424f780eb80f01fa43a4d2e2c6fe
Geethalakshmi Saravanan <43269109+GeethalakshmiSA@users.noreply.github.com>
2022-11-01 17:30:41 +0530

    Update D1-P004.py

--- Practice Problems/Basic Programs/D1-P004.py
+++ Practice Problems/Basic Programs/D1-P004.py
@@ -1,10 +1,20 @@
-# PROBLEM STATEMENT - Calculate Simple Interest
+# PROBLEM STATEMENT - Calculate Simple Interest & Compund Interest
 # Formula of Simple Interest = (Principle * Time * Rate)/100
+# Formula of Compound Interest = Amount - Principle where Amount =  Principle (1 + Rate/100) ** Time
+
+def simpleInterest(principle, timePeriod, rateOfInterest):
+    interest = (principle * timePeriod * rateOfInterest) / 100
+    return interest
+
+def compoundInterest(principle, timePeriod, rateOfInterest):
+    amount = principle * (1 + (rateOfInterest/100)) ** timePeriod
+    interest = amount - principle
+    return interest
+
 
 principle = float(input("Enter the principle amount = "))
 timePeriod = float(input("Enter the time period taken in years = "))
 rateOfInterest = float(input("Enter the rate of interest = "))
 
-simpleInterest = (principle * timePeriod * rateOfInterest) / 100
-
-print("Simple Interest is ", simpleInterest)
+print("Simple Interest is ", simpleInterest(principle, timePeriod, rateOfInterest))
+print("Compound Interest is ", compoundInterest(principle, timePeriod, rateOfInterest))
