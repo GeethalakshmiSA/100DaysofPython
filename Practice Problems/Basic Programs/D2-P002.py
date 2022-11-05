dd66e85c3066fc7b3b915a31f0384b235834e797
Geethalakshmi Saravanan <43269109+GeethalakshmiSA@users.noreply.github.com>
2022-11-05 20:49:49 +0530

    Create D2-P002.py

---
+++ Practice Problems/Basic Programs/D2-P002.py
@@ -0,0 +1,34 @@
+# PROBLEM STATEMENT - Fibonacci
+
+# Model 1 : Fibonacci Series 
+
+def fibo(n):
+    fibArr = [0,1]
+    if n > 0:
+        a = fibArr[0] 
+        b = fibArr[1]
+        for i in range(2, n):
+            c = a + b 
+            a = b 
+            b = c 
+            fibArr.append(b)
+        return fibArr
+
+num = int(input("Enter the number to print the fibonacci series : "))
+print("Fibonacci series : ", fibo(num))
+
+# Model 2 : Finding fibonacci number using recursion
+
+def fibo(n):  
+    if n > 0:
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1 
+        else:
+            return fibo(n-1) + fibo(n-2)
+    else:
+        return "Invalid Input"
+        
+num = int(input("Enter the number to find the fibonacci from the series : "))
+print("Fibonacci number from the given input : ", fibo(num))
