5c90b2a5e0377a0bb5fd018a2d3526ed9e7f8ad3
Geethalakshmi Saravanan <43269109+GeethalakshmiSA@users.noreply.github.com>
2022-11-05 21:22:25 +0530

    Delete Practice Problems/Basic Programs/D2-P003 directory

--- Practice Problems/Basic Programs/D2-P003/D2-P003.py
+++
@@ -1,9 +0,0 @@
- # PROBLEM STATEMENT - To find the ASCII value of the given input
-  
-  # Model 1 : Using In-built function ORD()
-  
-character = input("Enter the character to find its ASCII value : ")
-if len(character) == 1:
-    print("The ASCII value of the character", character, "is ", ord(character))
-else:
-    print("Invalid Input! Please enter only one character and not a sequence of characters")
