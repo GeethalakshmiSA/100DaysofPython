3ccb098913b3d78c0d7d659cb85c741a88e69888
Geethalakshmi Saravanan <43269109+GeethalakshmiSA@users.noreply.github.com>
2022-11-08 09:03:59 +0530

    Update D3-P003.py

--- Practice Problems/Arrays/D3-P003.py
+++ Practice Problems/Arrays/D3-P003.py
@@ -18,3 +18,40 @@ for i in range(0, n):
 r = int(input("Number of elements to rotate ? "))
 print("Array after rotation is ", rotate(arr, r))
 
+# Model 2 : Using temp array and without remove function
+
+def rotate(arr, d, n):
+    temp = []
+    i=0
+    while (i<d):
+        temp.append(arr[i])
+        i += 1
+    i=0
+    while (d<n):
+        arr[i] = arr[d]
+        i += 1 
+        d += 1 
+    arr[ : ] = arr[:i]+temp
+    return arr
+
+arr = []
+n = int(input("Enter the number of elements in the array : "))
+print("Enter the elements in the array")
+for i in range(0, n):
+    arr.append(int(input()))
+r = int(input("Number of elements to rotate ? "))
+print("Array after rotation is ", rotate(arr, r, len(arr)))
+
+# Model 3 : Using Slicing approach
+
+def rotate(arr, d, n):
+    arr[ : ] = arr[d:n] + arr[0:d]
+    return arr
+
+arr = []
+n = int(input("Enter the number of elements in the array : "))
+print("Enter the elements in the array")
+for i in range(0, n):
+    arr.append(int(input()))
+r = int(input("Number of elements to rotate ? "))
+print("Array after rotation is ", rotate(arr, r, len(arr)))
